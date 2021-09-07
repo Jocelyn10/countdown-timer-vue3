2bd84062fe49bf6f513b190f8c7e16064b2ffd01
Jocelyn <jocelyn@kinshasadigital.com>
2021-09-07 12:42:21 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>
+    {{ diff.year }} years, {{ diff.month }} months, {{ diff.day }} days,
+    {{ diff.hour }} hours, {{ diff.minute }} minute, {{ diff.second }} seconds
+    until
+    {{ formatDate(futureDate) }}
+  </p>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const futureDate = new Date(2050, 0, 1);
+const getDateDiff = (date1, date2) => {
+  const diff = new Date(date2.getTime() - date1.getTime());
+  return {
+    year: diff.getUTCFullYear() - 1970,
+    month: diff.getUTCMonth(),
+    day: diff.getUTCDate() - 1,
+    hour: diff.getUTCHours(),
+    minute: diff.getUTCMinutes(),
+    second: diff.getUTCSeconds(),
+  };
+};
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      futureDate,
+      diff: {},
+      timer: undefined,
+    };
+  },
+  methods: {
+    getDiff() {
+      this.diff = getDateDiff(new Date(), futureDate);
+    },
+    formatDate(date) {
+      let d = new Date(date),
+        month = (d.getMonth() + 1).toString(),
+        day = d.getDate().toString(),
+        year = d.getFullYear().toString();
+      if (month.length < 2) month = "0" + month;
+      if (day.length < 2) day = "0" + day;
+      return [year, month, day].join("-");
+    },
+  },
+  beforeMount() {
+    this.timer = setInterval(this.getDiff, 1000);
+  },
+  beforeUnmount() {
+    clearInterval(this.timer);
+  },
+};
 </script>
 
 <style>
